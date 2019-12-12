68695e77b20220b067604713262dc873cb5143e2
jismyjose9 <58158527+jismyjose9@users.noreply.github.com>
2019-12-12 12:44:39 +0530

    Wrapper class Unboxing

---
+++ Unboxing.java
@@ -0,0 +1,14 @@
+//program for unboxing
+package wrapper;
+
+public class Unboxing {
+	public static void main(String args[]) {
+	//converting integer to int
+	Integer a=new Integer(10);
+	int i=a.intValue();
+	int j=i;
+	System.out.println(a+" "+i+" "+j);
+
+       }
+	}
+
