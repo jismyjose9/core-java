00767b2a2549fbe71b82eb52e7e0c27b61d36af5
jismyjose9 <58158527+jismyjose9@users.noreply.github.com>
2019-12-12 12:43:27 +0530

    Wrapper class Autoboxing

---
+++ Autoboxing.java
@@ -0,0 +1,16 @@
+//program for autoboxing
+package wrapper;
+
+public class Autoboxing {
+	public static void main(String args[]) {
+		Integer i=20;//converting int into Integer
+		int j=i;
+		System.out.println(i+" "+j);
+
+	       }
+		}
+
+	
+	
+
+
