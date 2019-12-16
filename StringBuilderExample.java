c8637f78f2a004794a2f275121698b3ef6178fbc
jismyjose9 <58158527+jismyjose9@users.noreply.github.com>
2019-12-16 14:17:19 +0530

    Program to the example of string builder

---
+++ StringBuilderExample.java
@@ -0,0 +1,24 @@
+package StringBuilder;
+public class StringBuilderExample {
+	public static void main(String args[])
+	{
+		StringBuilder s1=new StringBuilder(" Hello");
+		StringBuilder s2=new StringBuilder(" World");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s1.hashCode());
+		System.out.println(s2.hashCode());
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s1.append(s2));//append
+		System.out.println(s1); 
+		System.out.println(s2);
+		System.out.println(s1.hashCode());
+		System.out.println(s2.hashCode());
+		
+		
+		
+	}
+	
+
+}
