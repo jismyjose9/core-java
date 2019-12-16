3ef8ec1c077834cc11206bc0d6121bb65d2d33d0
jismyjose9 <58158527+jismyjose9@users.noreply.github.com>
2019-12-16 15:04:26 +0530

    Example of StringTokenizer

---
+++ StringTokenizerExample.java
@@ -0,0 +1,26 @@
+package StringTokenizer;
+import java.util.StringTokenizer;
+public class StringTokenizerExample {
+	public static void main(String[] args)
+	{
+		String str="I , am , going , to , split , this, string by,space";
+		StringTokenizer st=new StringTokenizer(str);
+		System.out.println("split by space");
+		while (st.hasMoreElements())
+	
+	{
+		System.out.println(st.nextElement());
+	}
+		
+		System.out.println();
+		System.out.println("split by ,");
+		StringTokenizer st2=new StringTokenizer(str, ",");
+		while (st2.hasMoreElements())
+		{
+			System.out.println(st2.nextElement());
+		}
+		
+	
+
+}
+}
