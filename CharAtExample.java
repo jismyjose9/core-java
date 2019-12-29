9c02e6bfb613bedac879aa83d1e261ce33982071
jismyjose9 <58158527+jismyjose9@users.noreply.github.com>
2019-12-29 16:52:32 +0530

    Example of charAt() method

---
+++ CharAtExample.java
@@ -0,0 +1,14 @@
+package StringMethods;
+
+public class CharAtExample {
+	public static void main(String[] args)
+	{
+		String name="FACE@2.0";
+		char ch=name.charAt(10);//returns the char value at the 10th index
+		System.out.println(ch);//
+	}
+
+}
+
+
+
