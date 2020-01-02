f4b5e122f8f921418bdd269db63311afa5f981c5
jismyjose9 <58158527+jismyjose9@users.noreply.github.com>
2020-01-02 17:55:12 +0530

    Example of endsWith() method

---
+++ EndsWithExample1.java
@@ -0,0 +1,13 @@
+package StringMethods;
+
+public class EndsWithExample1 {
+	public static void main(String[] args)
+	{
+		String s1="Java by Face";
+		System.out.println(s1.endsWith("E"));
+		System.out.println(s1.endsWith("FACE"));
+		
+	}
+	
+
+}
