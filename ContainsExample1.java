ba8ea0dd57c44bf19f975408228342e90ca16a39
jismyjose9 <58158527+jismyjose9@users.noreply.github.com>
2020-01-02 17:51:55 +0530

    Example of contains() method

---
+++ ContainsExample1.java
@@ -0,0 +1,13 @@
+package StringMethods;
+
+public class ContainsExample1 {
+	public static void main(String[] args)
+	{
+		String name="what do you know about me";
+		System.out.println(name.contains("do you know"));//check "do you know" in the string
+		System.out.println(name.contains("about"));
+		System.out.println(name.contains("hello"));
+		
+	}
+
+}
