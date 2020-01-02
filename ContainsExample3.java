6431f0bca2e525dc83c79a44fdd0d1826020f376
jismyjose9 <58158527+jismyjose9@users.noreply.github.com>
2020-01-02 17:53:24 +0530

    Example of contains() method

---
+++ ContainsExample3.java
@@ -0,0 +1,16 @@
+package StringMethods;
+
+public class ContainsExample3 {
+	public static void main(String[] args)
+	{
+		String str="To learn java visit focusacademy.in";
+		if(str.contains("focusAcademy.in.com"))
+		{
+			System.out.println("This string contains focusacademy.in");
+			
+		}
+		else
+			System.out.println(" Result not found");
+	}
+
+}
