507c804a34395a1bbf045c71fa0d5b324fdcfea3
jismyjose9 <58158527+jismyjose9@users.noreply.github.com>
2020-01-04 09:43:47 +0530

    program for method overloading

--- polymorphism1.java
+++ polymorphism1.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 class Area
 {
 	public void findArea( int side) {
-	System.out.println("Area of the Square is "+(side*side));
+	System.out.println("Area of the Square is "+(side*side));//calcuiate area of square
 }
 	public void findArea( int length ,int breadth) {
-		System.out.println("Area of the Rectangle is "+(length*breadth));
+		System.out.println("Area of the Rectangle is "+(length*breadth));//calcuiate area of Rectangle
 		
 	}
 	public void findArea( float base ,float height) {
-		System.out.println("Area of the Triangle is "+((base*height)/2));
+		System.out.println("Area of the Triangle is "+((base*height)/2));//calcuiate area of Triangle
 	}
 	public void findArea(float radius) {
-		System.out.println("Area of the circle is "+((float)(3.14*radius*radius)));
+		System.out.println("Area of the circle is "+((float)(3.14*radius*radius)));//calcuiate area of circle
 		
 	}
 }
