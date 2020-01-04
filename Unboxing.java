1ca6dd8e365cf809481e986de2fe5fb995c90418
jismyjose9 <58158527+jismyjose9@users.noreply.github.com>
2020-01-04 12:53:32 +0530

    Update Unboxing.java

--- Unboxing.java
+++ Unboxing.java
@@ -5,7 +5,7 @@ public class Unboxing {
 	public static void main(String args[]) {
 	//converting integer to int
 	Integer a=new Integer(10);//creating object in integer
-	int i=a.intValue();
+	int i=a.intValue();//converting Integer into int
 	int j=i;
 	System.out.println(a+" "+i+" "+j);
 
